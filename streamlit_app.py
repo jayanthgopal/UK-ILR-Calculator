782d845b55a1f57ee86ca4fc7096397e35778e82
Jayanth G <40367036+jayanthgopal@users.noreply.github.com>
2023-07-15 18:31:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ def calculate_date(date, days):
   return new_date.strftime('%Y-%m-%d')
 
 def main():
+  st.title('ILR Calculator')
   date = st.date_input('Enter a date', value='2023-07-15')
   new_date = calculate_date(date, 1799)
   st.write('The date 1799 days from {} is {}'.format(date, new_date))
