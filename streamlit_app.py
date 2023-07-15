229f2c4682988089c93a36d6f4a62ecc09512f45
Jayanth G <40367036+jayanthgopal@users.noreply.github.com>
2023-07-15 19:59:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,16 @@
 import streamlit as st
 import datetime
 
-# def calculate_date(date, days):
-#   new_date = datetime.datetime.strptime(date, '%Y-%m-%d') + datetime.timedelta(days=days)
-#   return new_date.strftime('%Y-%m-%d')
+def calculate_date(date, days):
+  new_date = datetime.datetime.strptime(date, '%Y-%m-%d') + datetime.timedelta(days=days)
+  return new_date.strftime('%Y-%m-%d')
 
 def main():
   st.title('ILR Calculator')
   d = st.date_input("Date of Visa")
   st.write('Visa Date:', d)
-  # new_date = calculate_date(date, 1799)
-  # st.write('The date 1799 days from {} is {}'.format(date, new_date))
+  new_date = calculate_date(date, 1799)
+  st.write('ILP Eligibility from {}'.format(new_date))
 
 if __name__ == '__main__':
   main()
