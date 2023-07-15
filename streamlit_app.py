a4092c531bb15e7b9c9fba0ea95cd22bd4e03828
Jayanth G <40367036+jayanthgopal@users.noreply.github.com>
2023-07-15 19:57:15 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,10 @@ import datetime
 
 def main():
   st.title('ILR Calculator')
-  date = st.date_input('Enter a date', value='2023-07-15')
+  st.date_input(
+      "Date of Visa",
+      datetime.date(2019, 7, 6))
+  st.write('Visa Date:', d)
   # new_date = calculate_date(date, 1799)
   # st.write('The date 1799 days from {} is {}'.format(date, new_date))
 
