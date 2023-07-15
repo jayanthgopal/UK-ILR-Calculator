06bcca63d3bd86ead58067ae7d6b17388953f2ce
Jayanth G <40367036+jayanthgopal@users.noreply.github.com>
2023-07-15 20:27:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,30 +16,6 @@ def main():
     st.write('Visa Date: {}'.format(visa_date))  
     new_date = calculate_date(str(visa_date), 1799)
     st.write('ILR Eligibility from {}'.format(new_date))
-    
-    # Create an empty dataframe
-    data = pd.DataFrame(columns=["Random"])
-    st.text("Original dataframe")
-    
-    # with every interaction, the script runs from top to bottom
-    # resulting in the empty dataframe
-    st.dataframe(data) 
-    
-    # persist state of dataframe
-    session_state = SessionState.get(df=data)
-    
-    # random value to append; could be a num_input widget if you want
-    random_value = np.random.randn()
-    
-    if st.button("Append random value"):
-        # update dataframe state
-        session_state.df = session_state.df.append({'Random': random_value}, ignore_index=True)
-        st.text("Updated dataframe")
-        st.dataframe(session_state.df)
-    
-    # still empty as state is not persisted
-    st.text("Original dataframe")
-    st.dataframe(data)
 
 if __name__ == '__main__':
   main()
