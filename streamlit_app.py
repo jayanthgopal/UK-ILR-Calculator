0cbf1ae63372261e33f8b04bd5c752301adbded0
Jayanth G <40367036+jayanthgopal@users.noreply.github.com>
2023-07-15 18:30:31 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,14 @@
 import streamlit as st
-import pandas as pd
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
+import datetime
 
-df
+def calculate_date(date, days):
+  new_date = datetime.datetime.strptime(date, '%Y-%m-%d') + datetime.timedelta(days=days)
+  return new_date.strftime('%Y-%m-%d')
+
+def main():
+  date = st.date_input('Enter a date', value='2023-07-15')
+  new_date = calculate_date(date, 1799)
+  st.write('The date 1799 days from {} is {}'.format(date, new_date))
+
+if __name__ == '__main__':
+  main()
